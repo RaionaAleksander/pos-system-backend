c3d8a3d3d805110253b5d0d191c3e3e0cf74c74e
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-04-01 12:57:29 +0300

    Initial Spring POS system backend project

---
+++ src/test/java/com/aleksander/pos/PosSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksander.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
