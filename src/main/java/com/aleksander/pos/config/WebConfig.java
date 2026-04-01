be485ac3ccf43ce4c3f913bf5a67e3448580a336
Aleksander Ontin <aleksander.ontin@gmail.com>
2026-04-02 00:48:35 +0300

    Config: add basic security and CORS configuration

---
+++ src/main/java/com/aleksander/pos/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.aleksander.pos.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.micrometer.common.lang.NonNull;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
